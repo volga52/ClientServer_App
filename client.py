e58ad9c6766052d83f1908e5f24ebe809a15dd9b
Евгений <volga52@bk.ru>
2021-08-04 06:57:32 +0300

    update project for classes

--- client.py
+++ client.py
@@ -9,63 +9,68 @@ from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
 from common.utils import get_message, send_message
 
 
-def create_presence(account_name='Guest'):
-    '''
-    Функция генерирует запрос о присутствии клиента
-    :param account_name:
-    :return:
-    '''
-    # {'action': 'presence', 'time': 1573760672.167031, 'user': {'account_name': 'Guest'}}
-    out = {
-        ACTION: PRESENCE,
-        TIME: time.time(),
-        USER: {
-            ACCOUNT_NAME: account_name
+class ClintConnect:
+    def __init__(self, start_param):
+        self.sys_param = start_param
+
+    def create_presence(self, account_name='Guest'):
+        '''
+        Функция генерирует запрос о присутствии клиента
+        :param account_name:
+        :return:
+        '''
+        # {'action': 'presence', 'time': 1573760672.167031, 'user': {'account_name': 'Guest'}}
+        out = {
+            ACTION: PRESENCE,
+            TIME: time.time(),
+            USER: {
+                ACCOUNT_NAME: account_name
+            }
         }
-    }
-    return out
+        return out
+
+    def process_ans(self, message):
+        '''
+        Функция разбирает ответ сервера
+        :param message:
+        :return:
+        '''
+        if RESPONSE in message:
+            if message[RESPONSE] == 200:
+                return '200 : OK'
+            return f'400 : {message[ERROR]}'
+        raise ValueError
+
+    def connect(self):
+        '''Загружаем параметы коммандной строки'''
+        # client.py 192.168.1.2 8079
+        try:
+            server_address = self.sys_param[1]
+            server_port = int(self.sys_param[2])
+            if server_port < 1024 or server_port > 65535:
+                raise ValueError
+
+        except IndexError:
+            server_address = DEFAULT_IP_ADDRESS
+            server_port = DEFAULT_PORT
+        except ValueError:
+            print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
+            sys.exit(1)
+
+        # Инициализация сокета и обмен
+
+        transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transport.connect((server_address, server_port))
+        message_to_server = self.create_presence()
+        send_message(transport, message_to_server)
+        try:
+            answer = self.process_ans(get_message(transport))
+            print(answer)
+        except (ValueError, json.JSONDecodeError):
+            print('Не удалось декодировать сообщение сервера.')
+
+# if __name__ == '__main__':
+#     main()
 
 
-def process_ans(message):
-    '''
-    Функция разбирает ответ сервера
-    :param message:
-    :return:
-    '''
-    if RESPONSE in message:
-        if message[RESPONSE] == 200:
-            return '200 : OK'
-        return f'400 : {message[ERROR]}'
-    raise ValueError
-
-
-def main():
-    '''Загружаем параметы коммандной строки'''
-    # client.py 192.168.1.2 8079
-    try:
-        server_address = sys.argv[1]
-        server_port = int(sys.argv[2])
-        if server_port < 1024 or server_port > 65535:
-            raise ValueError
-    except IndexError:
-        server_address = DEFAULT_IP_ADDRESS
-        server_port = DEFAULT_PORT
-    except ValueError:
-        print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
-        sys.exit(1)
-
-    # Инициализация сокета и обмен
-
-    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    transport.connect((server_address, server_port))
-    message_to_server = create_presence()
-    send_message(transport, message_to_server)
-    try:
-        answer = process_ans(get_message(transport))
-        print(answer)
-    except (ValueError, json.JSONDecodeError):
-        print('Не удалось декодировать сообщение сервера.')
-
-
-if __name__ == '__main__':
-    main()
+ClintConnect(sys.argv).connect()
